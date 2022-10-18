250bc101f6801e8399269aa5ecaa4923a37b6bdd
Allan Agaba <aglanx@gmail.com>
2022-10-18 16:10:19 +0000

    Updated the code

--- main.h
+++ main.h
@@ -4,6 +4,20 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct flags - struct
+ * @plus: for the plus
+ * @space: for the space
+ * @hash: for the hash
+ */
+
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
